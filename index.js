d464d1ac12b96c405273de69ce738e30cbfb8a09
Ruhan <ruhan_350@hotmail.com>
2024-11-17 16:19:41 -0300

    implementa função registrarHoraTrabalhada

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const listaFuncionarios = []
 
-const adicionarFuncionario = (id, nome, cargo, taxaHoraria) => {
+function adicionarFuncionario(id, nome, cargo, taxaHoraria){
     let funcionario = {
         id: id,
         nome: nome,
@@ -12,5 +12,22 @@ const adicionarFuncionario = (id, nome, cargo, taxaHoraria) => {
     listaFuncionarios.push(funcionario)
 }
 
+function registrarHoraTrabalhada(idFuncionario, numHoras){
+    listaFuncionarios.map((funcionario) => {
+        if(funcionario.id === idFuncionario){
+            funcionario.horasTrabalhadas.push(numHoras)
+        }
+    })
+}
+
+
+
 adicionarFuncionario(1, 'João', 'Analista de Sistemas', 100)
+adicionarFuncionario(2, 'Maria', 'Gerente de Projetos', 150)
+adicionarFuncionario(3, 'José', 'Desenvolvedor', 80)
+adicionarFuncionario(4, 'Ana', 'Analista de Sistemas', 100)
+registrarHoraTrabalhada(1, 8)
+registrarHoraTrabalhada(2, 8)
+registrarHoraTrabalhada(3, 8)
+registrarHoraTrabalhada(4, 6)
 console.log(listaFuncionarios)
